a076ab47d056397a6341dcb635c8a30d73196fc9
“bill” <salmanmansoorsadiq@gmail.com>
2019-01-09 17:30:33 +0500

    Commititng Frontend code to master

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    hmr       : false,
+    //Live Environment
+    //serviceUrl: 'https://api.healthport.io/',
+    //Staging Environment
+    serviceUrl: 'http://api.staging.healthport.io/'
+};

8952c572e95dd285a2760c38e994b5ebbbcd61fa
AliRizvi <ali.sajjad@ideofuzion.com>
2019-01-15 13:14:05 +0500

    Committing Airdrop Functionality with Try Catch

--- apis/cron/airdrop.js
+++ apis/cron/airdrop.js
@@ -18,47 +18,52 @@ var options = {
 };
 
 var task = cron.schedule('*/15 * * * *', async () => {
-    var promisesArray = [];
-    //DB Queries
-    [err, airDropUsersCount] = await utils.to(db.models.air_drop_users.count());
-    if (err) {
-        console.log(err);
-        return;
-    }
-    [err, rewardObj] = await utils.to(db.models.reward_conf.findAll({
-        where: {
-            reward_type: rewardEnum.AIRDROPREWARD
+    try {
+        var promisesArray = [];
+        //DB Queries
+        [err, airDropUsersCount] = await utils.to(db.models.air_drop_users.count());
+        if (err) {
+            console.log(err);
+            return;
         }
-    }));
-    if (err) {
-        console.log(err);
-        return;
-    }
-    if (airDropUsersCount >= rewardObj[0].max_users) {
-        return;
-    }
-    if (new Date(rewardObj[0].reward_end_date) < new Date()) {
-        return;
-    }
-    //Getting Transactions which are on TRON Network
-    options.uri = apiUrlForTransfers;
-    var response = await rp(options)
-    // Filtering Transactions whose Transaction amount is greater or equal to 100 TRX
-    var filteredTransactions = response.data.filter(x => x.amount >= rewardObj[0].reward_amount * Math.pow(10, process.env.DECIMALS));
-    for (var i = 0; i < filteredTransactions.length; i++) { // Loop For Filtered Transactions
-        //Getting Detail of Address from the TRON Network API
-        options.uri = `${apiUrlForAddressDetails}${filteredTransactions[i].transferToAddress}`;
-        //Pushing Request Promise in Promises Array
-        promisesArray.push(rp(options));
-    }// End of FOR
-    //Resolving all Promises which are in promisesArray
-    var promises_response = await Promise.all(promisesArray);
-    var filtered_promises_array = filterBalances(promises_response, process.env.TRON_TOKEN_KEY);
-    console.log(filtered_promises_array);
-    for (var i = 0; i < filtered_promises_array.length; i++) {
-        if (filtered_promises_array[i].address) {
-            await sendEHRTokensToAirDropUsers(filtered_promises_array[i].address, rewardObj[0].reward_amount);
+        [err, rewardObj] = await utils.to(db.models.reward_conf.findAll({
+            where: {
+                reward_type: rewardEnum.AIRDROPREWARD
+            }
+        }));
+        if (err) {
+            console.log(err);
+            return;
         }
+        if (airDropUsersCount >= rewardObj[0].max_users) {
+            return;
+        }
+        if (new Date(rewardObj[0].reward_end_date) < new Date()) {
+            return;
+        }
+        //Getting Transactions which are on TRON Network
+        options.uri = apiUrlForTransfers;
+        var response = await rp(options)
+        // Filtering Transactions whose Transaction amount is greater or equal to 100 TRX
+        var filteredTransactions = response.data.filter(x => x.amount >= rewardObj[0].reward_amount * Math.pow(10, process.env.DECIMALS));
+        for (var i = 0; i < filteredTransactions.length; i++) { // Loop For Filtered Transactions
+            //Getting Detail of Address from the TRON Network API
+            options.uri = `${apiUrlForAddressDetails}${filteredTransactions[i].transferToAddress}`;
+            //Pushing Request Promise in Promises Array
+            promisesArray.push(rp(options));
+        }// End of FOR
+        //Resolving all Promises which are in promisesArray
+        var promises_response = await Promise.all(promisesArray);
+        var filtered_promises_array = filterBalances(promises_response, process.env.TRON_TOKEN_KEY);
+        console.log(filtered_promises_array);
+        for (var i = 0; i < filtered_promises_array.length; i++) {
+            if (filtered_promises_array[i].address) {
+                await sendEHRTokensToAirDropUsers(filtered_promises_array[i].address, rewardObj[0].reward_amount);
+            }
+        }
+    }
+    catch (exp) {
+        console.log(exp);
     }
 }, {
         scheduled: false

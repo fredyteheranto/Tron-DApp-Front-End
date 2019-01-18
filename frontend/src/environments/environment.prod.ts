a949053e5ca6a2825d9bb2133fd37acd81e2f18e
“bill” <salmanmansoorsadiq@gmail.com>
2019-01-18 15:39:12 +0500

    new build at 1538 prod url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     hmr       : false,
     //Live Environment
-    //serviceUrl: 'https://api.healthport.io/',
+    serviceUrl: 'https://api.healthport.io/'
     //Staging Environment
-    serviceUrl: 'http://api.staging.healthport.io/'
+    /* serviceUrl: 'http://api.staging.healthport.io/' */
 };

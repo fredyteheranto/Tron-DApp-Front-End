0c3f79985edaab8b0603788dfb907ed7c6a74529
“bill” <salmanmansoorsadiq@gmail.com>
2019-02-11 20:39:45 +0500

    remove environment folder | update tslint and tsconfig

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-    hmr       : false,
-    //Live Environment
-    serviceUrl: 'https://api.healthport.io/'
-    //Staging Environment
-    /* serviceUrl: 'http://api.staging.healthport.io/' */
-};

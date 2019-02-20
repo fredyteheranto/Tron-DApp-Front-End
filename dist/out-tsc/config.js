3229e29ebef5f874ab67f9343818569f169b9159
“bill” <salmanmansoorsadiq@gmail.com>
2019-02-20 21:15:36 +0500

    setting frontend code for ci

--- dist/out-tsc/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.username = "postgres";
-exports.password = "";
-exports.database = "healthportdb";
-exports.host = "localhost";
-exports.port = 5432;
-exports.dialect = "postgresql";
-//# sourceMappingURL=config.js.map
--- dist/out-tsc/src/environments/environment.hmr.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = {
-    production: false,
-    hmr: true
-};
-//# sourceMappingURL=environment.hmr.js.map
